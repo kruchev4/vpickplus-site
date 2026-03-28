f91b1b40dc449e57058c768bc9e67d1301f8c65f
kruchev4 <120105804+kruchev4@users.noreply.github.com>
2026-03-28 16:47:17 -0400

    Update main.js

--- game/Echoes/main.js
+++ game/Echoes/main.js
@@ -5,7 +5,7 @@
 /* ---------- Engine Data ---------- */
 import { RACES } from "./engine/data/races.js";
 import { CLASSES } from "./engine/data/classes.js";
-console.log("races imported");
+console.log("races imported - not!");
 
 window.RACES = RACES;
 window.CLASSES = CLASSES;
