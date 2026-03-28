36d36ab71634529c9cad285edb407ff2560b4bfb
kruchev4 <120105804+kruchev4@users.noreply.github.com>
2026-03-28 00:09:16 -0400

    Create stats.js

---
+++ game/Echoes/engine/data/stats.js
@@ -0,0 +1,8 @@
+export const STAT_DEFS = [
+  { key: "STR", name: "Strength" },
+  { key: "DEX", name: "Dexterity" },
+  { key: "CON", name: "Constitution" },
+  { key: "INT", name: "Intelligence" },
+  { key: "WIS", name: "Wisdom" },
+  { key: "CHA", name: "Charisma" },
+];
